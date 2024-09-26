a33590c09c04d06a5d1f1e73595f4508f85e299e
AKSHUVO94 <info.akshuvo@gmail.com>
2024-09-26 17:49:52 +0600

    4 to 8

--- Test.js
+++ Test.js
@@ -91,5 +91,5 @@
 
 // ------Math.random----- Default value less then 1 and gater then 0//   
 
-// var sum = Math.random()*100;
-// console.log(sum)
+var sum = Math.random()*100;
+console.log(sum)
--- assignment.js
+++ assignment.js
@@ -1,13 +1,33 @@
 // 1. What is variable ?
-
 // 1. Ans: variable is a Value that can change, depending on conditions or on information passed to the program.
 
 // 2. How to set variable ?
-
 // 2. Ans:  var Name= variable name;
-
 //          Like  var Name= "Shuvo";
 
 // 3. What is String types of variables ? How to write String types of variables?
+// 3. Ans: String types of variables is Single quotation (''), Double quotation ("") and backtik (``).
 
-// 3. Ans: String types of variables is Single quotation (''), Double quotation (""), `
+// 4. Number Types of variables ? How to write Number types of variables?
+// 4. Ans : Number types of variable is :var age = 30; var price = 99.99; and let integer = 42;   let floatNumber = 3.14;         
+
+// 5. Boolean Types of variables ? How to write Boolean types of variables?
+// 5. Ans : Boolean Types of variables is : var isActive = true; var isComplete = false;
+
+// 6. How to use toUpperCase () & toLowerCase () function?
+// 6.
+
+
+// 7. how much operator by javascript ?
+// 7. Ans :  + (Addition)                            var x = 10 + 5;  
+        //   - (Subtraction)                         var x = 10 - 5;
+        //   * (Multiplication)                      var x = 10 * 5;
+        //   / (Division)                            var x = 10 / 5;
+        //   % (Modulus — remainder of division)     var x = 10 % 5;
+
+ 
+// 8. How To Use  math.abs () ?
+// 8. Ans: 
+//         var Num = -50;
+//         var sum = Math.abs(Num1);
+//         console.log(sum)
